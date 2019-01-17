de19a60e821aa5a9b803d37e9e296b55bbb831bf
Milan V. Kecojević <staog@gmx.com>
2019-01-17 23:16:57 +0100

    Update balloons-poping.js

--- games/balloons-poping.js
+++ games/balloons-poping.js
@@ -1,99 +1,147 @@
 /* Made a few changes:
    1. Changed original scene
-   2. Replaced sticks with baloons
-   3. Replaced main caracter
-   4. Made it a bit slower
-   5.  
+   2. Replaced grass with spikes that can take life
+   3. Added spikes to the top edge
+   3. Replaced sticks with baloons
+   4. Replaced main caracter
+   5. Made it a bit slower
+   6. Added number of lifes
+   7. Added win scenario screen
+   8. Added game over conditional and screen
 */
 
-var Beaver = function(x, y) {
+var Robot = function(x, y) {
     this.x = x;
     this.y = y;
-    this.img = getImage("avatars/mr-pants");
-    this.sticks = 0;
+    this.img = getImage("avatars/robot_male_3");
+    this.balloons = 0;
+    this.lives = 3;
 };
 
-Beaver.prototype.draw = function() {
+Robot.prototype.draw = function() {
     fill(255, 0, 0);
     this.y = constrain(this.y, 10, height - 90);
     image(this.img, this.x, this.y, 40, 40);
 };
 
-Beaver.prototype.hop = function() { 
-    this.img = getImage("avatars/mr-pants");
+Robot.prototype.hop = function() {
+    this.img = getImage("avatars/robot_male_2");
     this.y -= 3;
 };
 
-Beaver.prototype.fall = function() {
-    this.img = getImage("avatars/mr-pants-with-hat");
+Robot.prototype.fall = function() {
+    this.img = getImage("avatars/robot_male_1");
     this.y += 4;
 };
 
-Beaver.prototype.checkForStickGrab = function(stick) {
-    if ((stick.x >= this.x && stick.x <= (this.x + 40)) &&
-        (stick.y >= this.y && stick.y <= (this.y + 40))) {
-        stick.y = -400;
-        this.sticks++;
+Robot.prototype.checkForBallonPop = function(bln) {
+    if ((bln.x >= this.x && bln.x <= (this.x + 40)) &&
+        (bln.y >= this.y && bln.y <= (this.y + 40))) {
+        bln.y = -400;
+        this.balloons++;
     }
 };
 
-var Stick = function(x, y) {
+var Balloon = function(x, y) {
     this.x = x;
     this.y = y;
 };
 
-Stick.prototype.draw = function() {
+Balloon.prototype.draw = function() {
     fill(245, 139, 10);
     rectMode(CENTER);
     ellipse(this.x, this.y, 25, 30);
 };
 
-var beaver = new Beaver(200, 300);
+var youWin = function() {
+        fill(238, 245, 24);
+        rect(0, 0, width * 2, height * 2);
+        fill(10, 10, 10);
+        textSize(36);
+        text("YOU WIN!!!", 100, 200);
+        };
 
-var sticks = [];
-for (var i = 0; i < 40; i++) {  
-    sticks.push(new Stick(i * 40 + 300, random(20, 260)));
+var gameOver = function() {
+            fill(255, 0, 0);
+            rect(0, 0, width * 2, height * 2);
+            textSize(40);
+            fill(0, 0, 0);
+            text("GAME OVER", 80, 160);
+        };
+
+var robot = new Robot(100, 100);
+
+var balloons = [];
+for (var i = 0; i < 40; i++) {
+    balloons.push(new Balloon(i * 40 + 300, random(50, 280)));
 }
 
-var grassXs = [];
-for (var i = 0; i < 25; i++) { 
-    grassXs.push(i*20);
+var spikes = [];
+for (var i = 0; i < 25; i++) {
+    spikes.push(i*20);
 }
 
 draw = function() {
-    
+
     // static
-    background(37, 58, 59);
+    background(31, 173, 209);
     fill(130, 79, 43);
     rectMode(CORNER);
     rect(0, height*0.90, width, height*0.10);
-    
-    for (var i = 0; i < grassXs.length; i++) {
-        image(getImage("cute/GrassBlock"), grassXs[i], height*0.81, 20, 44);
-        grassXs[i] -= 1;
-        if (grassXs[i] <= -20) {
-            grassXs[i] = width;
+
+    for (var i = 0; i < spikes.length; i++) {
+        image(getImage("cute/RampWest"), spikes[i], height*0.81, 20, 70);
+        spikes[i] -= 1;
+        if (spikes[i] <= -20) {
+            spikes[i] = width;
         }
     }
     
-    for (var i = 0; i < sticks.length; i++) {
-        sticks[i].draw();
-        beaver.checkForStickGrab(sticks[i]);
-        sticks[i].x -= 1;
+    for (var i = 0; i < spikes.length; i++) {
+        image(getImage("cute/RampWest"), spikes[i], height*-0.09, 20, 60);
+        spikes[i] -= 1;
+        if (spikes[i] <= -20) {
+            spikes[i] = width;
+        }
     }
-    
+
+    for (var i = 0; i < balloons.length; i++) {
+        balloons[i].draw();
+        robot.checkForBallonPop(balloons[i]);
+        balloons[i].x -= 1;
+    }
+
     textSize(18);
-    text("Score: " + beaver.sticks, 20, 30);
-    
-    if (beaver.sticks/sticks.length >= 0.95) {
-        textSize(36);
-        text("YOU WIN!!!!", 100, 200);
+    fill(10, 10, 10);
+    text("Score: " + robot.balloons, 20, 50);
+    text("Lives: " + robot.lives, 310, 50);
+
+    if (robot.balloons/balloons.length >= 0.90) {
+        return youWin();
     }
-    
+
     if (keyIsPressed && keyCode === 0) {
-        beaver.hop();
+        robot.hop();
     } else {
-        beaver.fall();
+        robot.fall();
     }
-    beaver.draw();
+    
+    if (robot.y < 8 || robot.y > 313 && robot.lives > 0) {
+        fill(8, 8, 8);
+        rect(0, 0, width * 2, height * 2);
+        fill(255, 0, 0);
+        textSize(36);
+        text("Ooouuch...", 100, 150);
+        text("That hurts!", 90, 200);
+        robot.img = getImage("avatars/robot_female_1");
+        robot.lives -= 1;
+        robot.x = 100;
+        robot.y = 100;
+    }
+    else if (robot.lives === 0) {
+        return gameOver(); 
+    }
+    
+    robot.draw();
 };
+
